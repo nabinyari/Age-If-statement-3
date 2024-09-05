2a67edff172235e7d9095dec897a4faa808a773c
Nabin Yari (Atit) <nabinyarijr@gmail.com>
2024-09-06 01:47:06 +0545

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception
+    {
+        int age = 34;
+        if(age > 0 && age <= 17)
+        {
+            System.out.println("Your age is " + age + ". You are under age.");
+        }
+        else if(age >=18 && age <=60 )
+        {
+            System.out.println("Your age is " + age +". You are young.");
+        }
+        else
+        {
+            System.out.println("Your age is " + age + ". You have to retire and take a rest.");
+        }
+    }
+}
