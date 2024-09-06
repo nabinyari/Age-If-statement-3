34488873354d7dc9a806519335f4b61c886caa15
Nabin Yari (Atit) <nabinyarijr@gmail.com>
2024-09-06 14:28:10 +0545

    I just change the variable value and add fine.

--- src/App.java
+++ src/App.java
@@ -1,14 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception
     {
-        int age = 68;
+        int age = 36;
         if(age > 0 && age <= 17)
         {
             System.out.println( age + ". You are under age.");
         }
         else if(age >=18 && age <=60 )
         {
-            System.out.println("Your age is " + age +". You are young.");
+            System.out.println("Your age is " + age +". You are young and fine.");
         }
         else
         {
